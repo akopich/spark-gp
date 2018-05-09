2261389b6629dd86ef5c5dd8df55b6b5033c8633
Valeriy Avanesov <avanesov@wias-berlin.de>
2018-05-09 23:56:09 +0200

    Docs for kernel interface and RBF kernel are added

--- src/main/scala/org/apache/spark/ml/regression/kernel/Kernel.scala
+++ src/main/scala/org/apache/spark/ml/regression/kernel/Kernel.scala
@@ -4,29 +4,78 @@ import breeze.linalg.{Transpose, DenseMatrix => BDM, DenseVector => BDV}
 import breeze.numerics._
 import org.apache.spark.ml.linalg.{Vector, Vectors}
 
-
+/**
+  * Trait defining the covariance function `k` of a Gaussian Process
+  *
+  * The kernel should be differentiable with respect to the hyperparemeters
+  *
+  */
 trait Kernel extends Serializable {
+  /**
+    * A vector of hyperparameters of the kernel
+    */
   var hyperparameters: BDV[Double]
 
+  /**
+    * Stores some portion of the training sample
+    */
+  var trainOption: Option[Array[Vector]]
+
+  /**
+    * Setter
+    *
+    * @param value
+    * @return this
+    */
   def setHyperparameters(value: BDV[Double]): this.type = {
     hyperparameters = value
     this
   }
 
-  var trainOption: Option[Array[Vector]]
-
+  /**
+    *
+    * @return boundaries lower and upper: lower <= hyperparameter <= upper (inequalities are element-wise).
+    */
   def hyperparameterBoundaries : (BDV[Double], BDV[Double])
 
+  /**
+    *
+    * @param vectors
+    * @return this
+    */
   def setTrainingVectors(vectors: Array[Vector]): this.type
 
+  /**
+    * `setTrainingVectors` should be called beforehand. Otherwise TrainingVectorsNotInitializedException is thrown.
+    * @return a matrix K such that `K_{ij} = k(trainingVectros(i), trainingVectros(j))`
+    */
   def trainingKernel(): BDM[Double]
 
+  /**
+    * `setTrainingVectors` should be called beforehand. Otherwise TrainingVectorsNotInitializedException is thrown.
+    * @return the diagonal of the matrix which would be returned by `trainingKernel`.
+    */
   def trainingKernelDiag(): Array[Double]
 
+  /**
+    *`setTrainingVectors` should be called beforehand. Otherwise TrainingVectorsNotInitializedException is thrown.
+    * @return a pair of a kernel K (returned by `trainingKernel`)
+    *         and an array of partial derivatives \partial K / \partial hyperparameters(i)
+    */
   def trainingKernelAndDerivative(): (BDM[Double], Array[BDM[Double]])
 
+  /**
+    * `setTrainingVectors` should be called beforehand. Otherwise TrainingVectorsNotInitializedException is thrown.
+    * @param test vectors (typically, those we want a prediction for)
+    * @return a matrix K of size `test.size * trainVectors.size` such that K_{ij} = k(test(i), trainVectors(j))
+    */
   def crossKernel(test: Array[Vector]): BDM[Double]
 
+  /**
+    * `setTrainingVectors` should be called beforehand. Otherwise TrainingVectorsNotInitializedException is thrown.
+    * @param test a vector (typically, the one we want a prediction for)
+    * @return a row-vector K such that K_{i} = k(test, trainVectors(i))
+    */
   def crossKernel(test: Vector): Transpose[BDV[Double]] = {
     val k = crossKernel(Array(test))
     k(0, ::)
@@ -36,6 +85,13 @@ trait Kernel extends Serializable {
 class TrainingVectorsNotInitializedException
   extends Exception("setTrainingVectors method should have been called first")
 
+/**
+  * Implements traditional RBF kernel `k(x_i, k_j) = exp(||x_i - x_j||^2 / sigma^2)`
+  *
+  * @param sigma
+  * @param lower
+  * @param upper
+  */
 class RBFKernel(sigma: Double,
                 private var lower: Double = 1e-6,
                 private var upper: Double = inf) extends Kernel {
@@ -92,8 +148,7 @@ class RBFKernel(sigma: Double,
 
 
   override def trainingKernelDiag(): Array[Double] = {
-    val train = trainOption.getOrElse(throw new TrainingVectorsNotInitializedException)
-    train.map(_ => 1d)
+    trainOption.getOrElse(throw new TrainingVectorsNotInitializedException).map(_ => 1d)
   }
 
   private def sqr(x: Double) = x * x
