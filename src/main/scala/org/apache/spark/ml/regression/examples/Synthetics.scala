dbfd402103192a841799f13aee7a7e3cad5cab64
Valeriy Avanesov <avanesov@wias-berlin.de>
2018-05-30 22:02:48 +0200

    Synthetic example illustrates the use of WhiteNoiseKernel.

--- src/main/scala/org/apache/spark/ml/regression/examples/Synthetics.scala
+++ src/main/scala/org/apache/spark/ml/regression/examples/Synthetics.scala
@@ -4,27 +4,32 @@ import breeze.linalg._
 import breeze.numerics._
 import org.apache.spark.ml.feature.LabeledPoint
 import org.apache.spark.ml.linalg.Vectors
-import org.apache.spark.ml.regression.kernel.RBFKernel
-import org.apache.spark.ml.regression.{GaussianProcessRegression, GreedilyOptimizingActiveSetProvider}
+import org.apache.spark.ml.regression.kernel.ScalarTimesKernel._
+import org.apache.spark.ml.regression.kernel.SummableKernel._
+import org.apache.spark.ml.regression.kernel.{RBFKernel, WhiteNoiseKernel}
+import org.apache.spark.ml.regression.{GaussianProcessRegression, RandomActiveSetProvider}
 
 object Synthetics extends App with GPExample {
   import spark.sqlContext.implicits._
 
   override def name = "Synthetics"
 
+  val noiseVar = 0.01
+  val g = breeze.stats.distributions.Gaussian(0, math.sqrt(noiseVar))
+
   val X = linspace(0, 1, length = 2000).toDenseMatrix
-  val Y = sin(X).toArray
+  val Y = sin(X).toArray.map(y => y + g.sample())
 
   val instances = spark.sparkContext.parallelize(X.toArray.zip(Y).map { case(v, y) =>
     LabeledPoint(y, Vectors.dense(Array(v)))}).toDF
 
   val gp = new GaussianProcessRegression()
-    .setKernel(() => new RBFKernel(0.1))
+    .setKernel(() => 1*new RBFKernel(0.1, 1e-6, 10) + WhiteNoiseKernel(0.5, 0, 1))
     .setDatasetSizeForExpert(100)
-    .setActiveSetProvider(GreedilyOptimizingActiveSetProvider)
-    .setActiveSetSize(10)
+    .setActiveSetProvider(RandomActiveSetProvider)
+    .setActiveSetSize(100)
     .setSeed(13)
     .setSigma2(1e-3)
 
-  cv(gp, instances, 8e-5)
+  cv(gp, instances, 0.11)
 }
